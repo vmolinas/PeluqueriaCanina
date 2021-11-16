029fcb7bf6be2aa35b6e33cca05f92152a674114
Victor Molinas <u.vmolinas@gmail.com>
2021-11-16 12:16:18 -0300

    Push inicial

---
+++ PeluqueriaCanina/build/generated-sources/ap-source-output/logica/Mascota_.java
@@ -0,0 +1,21 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-11-16T00:14:23")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, Integer> num_cliente;
+    public static volatile SingularAttribute<Mascota, String> nombre_mascota;
+    public static volatile SingularAttribute<Mascota, String> raza;
+    public static volatile SingularAttribute<Mascota, String> nombre_duenio;
+    public static volatile SingularAttribute<Mascota, String> color;
+    public static volatile SingularAttribute<Mascota, String> observaciones;
+    public static volatile SingularAttribute<Mascota, String> atencion_especial;
+    public static volatile SingularAttribute<Mascota, String> alergia;
+    public static volatile SingularAttribute<Mascota, String> celular_duenio;
+
+}
